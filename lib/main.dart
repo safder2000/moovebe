3524f3c09265eaa0fe78501638c55a24094d04be
safdu2000 <safdu2000@gmail.com>
2023-02-20 21:42:47 +0530

    login page ui..for now, moving on😶‍🌫️

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:moovebe/presentation/login_screen.dart';
 import 'package:moovebe/presentation/starting_screen.dart';
 
@@ -16,6 +17,8 @@ class MyApp extends StatelessWidget {
       title: 'moovebe',
       theme: ThemeData(
         primarySwatch: Colors.red,
+      ).copyWith(
+        textTheme: GoogleFonts.montserratTextTheme(Theme.of(context).textTheme),
       ),
       home: LoginPage(),
     );
--- lib/presentation/login_screen.dart
+++ lib/presentation/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
@@ -8,50 +9,52 @@ class LoginPage extends StatelessWidget {
     TextEditingController nameController = TextEditingController();
     TextEditingController passwordController = TextEditingController();
     return Scaffold(
-      body: ListView(
+      body: Column(
         children: [
-          Container(
-            height: 300,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              color: Colors.black,
-              image: DecorationImage(
-                image: AssetImage('lib/assets/Polygon_1.png'),
-                fit: BoxFit.fill,
-              ),
-            ),
+          //welcome graphics...top
+          WelcomeContainer(),
+          const SizedBox(
+            height: 45,
           ),
-          TextFormField(
+          //TODO: make corener rounded 😑
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: TextFormField(
+                decoration: const InputDecoration(
+                  fillColor: Colors.black12,
+                  filled: true,
+                  border: InputBorder.none,
+                  label: Center(child: Text('Enter Username')),
+                ),
+                controller: passwordController,
+                textAlign: TextAlign.center),
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: TextFormField(
               decoration: const InputDecoration(
                 fillColor: Colors.black12,
                 filled: true,
                 border: InputBorder.none,
-                label: Center(child: Text('Enter Username')),
+                // OutlineInputBorder(
+                //   borderSide:
+                //       const BorderSide(color: Color.fromARGB(31, 254, 0, 0)),
+                //   borderRadius: BorderRadius.circular(10.0),
+                // ),
+                label: Center(child: Text('Enter Password')),
               ),
               controller: passwordController,
-              textAlign: TextAlign.center),
-          const SizedBox(
-            height: 18,
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              fillColor: Colors.black12,
-              filled: true,
-              border: InputBorder.none,
-              // OutlineInputBorder(
-              //   borderSide:
-              //       const BorderSide(color: Color.fromARGB(31, 254, 0, 0)),
-              //   borderRadius: BorderRadius.circular(10.0),
-              // ),
-              label: Center(child: Text('Enter Password')),
             ),
-            controller: passwordController,
           ),
+          Spacer(),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 25),
+            padding: const EdgeInsets.symmetric(vertical: 40),
             child: SizedBox(
               height: 50,
-              width: MediaQuery.of(context).size.width * 0.8,
+              width: MediaQuery.of(context).size.width * 0.9,
               child: ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
@@ -72,6 +75,52 @@ class LoginPage extends StatelessWidget {
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class WelcomeContainer extends StatelessWidget {
+  const WelcomeContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      width: MediaQuery.of(context).size.width,
+      decoration: const BoxDecoration(
+        color: Colors.black,
+        image: DecorationImage(
+          image: AssetImage('lib/assets/Polygon_1.png'),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(35.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                'Welcome',
+                style: GoogleFonts.montserrat(
+                    color: Colors.white,
+                    fontSize: 43,
+                    fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                'Manage your Bus and Drivers',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+            ]),
       ),
     );
   }
